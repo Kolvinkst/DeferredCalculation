27fc0eb089babcedc14675a5defd3f0f1f4dd6e0
Kolvinkst <einbenutzer1998@gmail.com>
2023-12-19 10:25:32 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfObjects = new List<PrintWhenGetting>();
+            for (int i = 1; i < 5; i++)
+                listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+
+            Console.WriteLine("Set up the query");
+            var result =
+                from o in listOfObjects
+                select o.InstanceNumber;
+
+            var immediate = result.ToList();
+
+            Console.WriteLine("Run the foreach");
+            foreach (var number in immediate)
+            {
+                Console.WriteLine($"Writing #{number}");
+            }
+            Console.WriteLine("Press any key to exit");
+            char key = Console.ReadKey().KeyChar;
+        }
+    }
+}
